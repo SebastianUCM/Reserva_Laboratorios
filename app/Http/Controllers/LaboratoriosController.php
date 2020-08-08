0f4e067755656b690b3ef1d8ca27ff37aac0edec
SebastianUCM <s.cerpa.gutierrez@gmail.com>
2020-08-08 05:45:38 -0400

    Reserva_Laboratorios 1.5 Se Crea Tabla, Controlador y Vista para Laboratorios

---
+++ app/Http/Controllers/LaboratoriosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Laboratorios;
+use Illuminate\Http\Request;
+
+class LaboratoriosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Laboratorios  $laboratorios
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Laboratorios $laboratorios)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Laboratorios  $laboratorios
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Laboratorios $laboratorios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Laboratorios  $laboratorios
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Laboratorios $laboratorios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Laboratorios  $laboratorios
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Laboratorios $laboratorios)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_08_08_092354_create_laboratorios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaboratoriosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laboratorios', function (Blueprint $table) {
+            $table->increments('Id');
+            $table->string('Nombre');
+            $table->string('Carrera');
+            $table->integer('Capacidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laboratorios');
+    }
+}
