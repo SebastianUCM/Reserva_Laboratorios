3c141411343a5a47b76ac21b406135615b7cf87b
SebastianUCM <s.cerpa.gutierrez@gmail.com>
2020-08-11 12:23:07 -0400

    Reserva_Laboratorios 2.0.9 Se Crea Acceso en el navbar

--- resources/views/Usuarios/crear.blade.php
+++ resources/views/Usuarios/crear.blade.php
@@ -13,25 +13,60 @@ crear usuario
                     {{ csrf_field() }}
 
                         <div class="form-group row">
-                            <label for="Name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
+
+<!--email-->
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electronico') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+<!--rol-->
+<!--Contraseña-->
+
 
                         <div class="form-group row">
-                            <label for="Carrera" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
-                            
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
- 
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
-
+<!--V. Contraseña-->
                         <div class="form-group row">
-                            <label for="Capacidad" class="col-md-4 col-form-label text-md-right">{{ __('Rol') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
 
-                            
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
                         </div>
-
-
+<!--boton-->
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
