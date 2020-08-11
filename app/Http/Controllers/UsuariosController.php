730078ea8d271a6b350021d555e6dec4ff104df0
SebastianM1903 <seba.r.f.v@gmail.com>
2020-08-10 23:32:04 -0400

    Reserva_Laboratorios 2.0.6 Se crea el Create de Usuarios

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class UsuariosController extends Controller
 {
@@ -12,6 +13,20 @@ class UsuariosController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+
+
+
+
     public function index()
     {
         return view('usuarios.index');
@@ -35,7 +50,14 @@ class UsuariosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        $usuario = new User();
+        $usuario->Nombre =request('Nombre');
+        $usuario->email =request('email');
+        $usuario->password = request('password');
+        $usuario->rol = request('rol');
+        $usuario->save();
+        return redirect('/Usuarios');
     }
 
     /**
@@ -44,6 +66,8 @@ class UsuariosController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+
     public function show($id)
     {
         //
--- resources/views/Usuarios/crear.blade.php
+++ resources/views/Usuarios/crear.blade.php
@@ -14,21 +14,48 @@ crear usuario
 
                         <div class="form-group row">
                             <label for="Name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
-
-
+                            <div class="col-md-6">
+                                <input id="nombre" type="text" class="form-control @error('name') is-invalid @enderror" name="Nombre" value="{{ old('name') }}" required autocomplete="Nombre" autofocus placeholder="Ej: Viviana Villanueva">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="Carrera" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
-                            
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('name') is-invalid @enderror" name="email" value="{{ old('name') }}" required autocomplete="email" autofocus placeholder="Ej: VivianaV@gmail.com">
 
- 
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required autocomplete="password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="Capacidad" class="col-md-4 col-form-label text-md-right">{{ __('Rol') }}</label>
-
-                            
+                            <select name="rol">
+                                <option value="secretario/a">Secretario/a</option> 
+                                <option value="encargado/a" >Encargado/a</option>
+                                <option value="usuario">Usuario</option>
+                            </select>
                         </div>
 
 
